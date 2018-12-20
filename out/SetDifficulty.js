d367c55afade97826411a1036d14e19cbc240ae9
Mackenzie McClane <yuudaari@gmail.com>
2018-12-20 12:47:39 -0600

    2.7 v2

--- out/SetDifficulty.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiU2V0RGlmZmljdWx0eS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9TZXREaWZmaWN1bHR5LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
