63bc5a3bb12f20a97ceada64d82530ae49842193
Vaughn Royko <drathy@gmail.com>
2023-06-12 17:32:15 -0700

    Update for 2.13.2-beta

--- out/action/IAction.js
+++ out/action/IAction.js
@@ -1,3 +1,13 @@
+/*!
+ * Copyright 2011-2023 Unlok
+ * https://www.unlok.ca
+ *
+ * Credits & Thanks:
+ * https://www.unlok.ca/credits-thanks/
+ *
+ * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
+ * https://github.com/WaywardGame/types/wiki
+ */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -11,6 +21,7 @@ define(["require", "exports", "game/entity/action/IAction", "mod/Mod", "../IBala
     exports.defaultUsability = [IAction_1.ActionUsability.Ghost, IAction_1.ActionUsability.Paused, IAction_1.ActionUsability.Delayed, IAction_1.ActionUsability.Moving];
     class Actions {
     }
+    exports.default = Actions;
     __decorate([
         Mod_1.default.instance(IBalancingTools_1.BALANCING_TOOLS_ID)
     ], Actions, "BALANCING_TOOLS", void 0);
@@ -20,6 +31,5 @@ define(["require", "exports", "game/entity/action/IAction", "mod/Mod", "../IBala
     __decorate([
         Mod_1.default.instance("Debug Tools")
     ], Actions, "DEBUG_TOOLS", void 0);
-    exports.default = Actions;
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSUFjdGlvbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9hY3Rpb24vSUFjdGlvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0lBT2EsUUFBQSxnQkFBZ0IsR0FBc0IsQ0FBQyx5QkFBZSxDQUFDLEtBQUssRUFBRSx5QkFBZSxDQUFDLE1BQU0sRUFBRSx5QkFBZSxDQUFDLE9BQU8sRUFBRSx5QkFBZSxDQUFDLE1BQU0sQ0FBQyxDQUFDO0lBSXBKLE1BQXFCLE9BQU87S0FPM0I7SUFMdUI7UUFEdEIsYUFBRyxDQUFDLFFBQVEsQ0FBaUIsb0NBQWtCLENBQUM7MENBQ007SUFFaEM7UUFEdEIsYUFBRyxDQUFDLEdBQUcsQ0FBQyxvQ0FBa0IsQ0FBQzs4QkFDSTtJQUVUO1FBRHRCLGFBQUcsQ0FBQyxRQUFRLENBQWEsYUFBYSxDQUFDO3NDQUNPO0lBTmhELDBCQU9DIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSUFjdGlvbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9hY3Rpb24vSUFjdGlvbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7Ozs7O0dBU0c7Ozs7Ozs7Ozs7O0lBU1UsUUFBQSxnQkFBZ0IsR0FBc0IsQ0FBQyx5QkFBZSxDQUFDLEtBQUssRUFBRSx5QkFBZSxDQUFDLE1BQU0sRUFBRSx5QkFBZSxDQUFDLE9BQU8sRUFBRSx5QkFBZSxDQUFDLE1BQU0sQ0FBQyxDQUFDO0lBSXBKLE1BQXFCLE9BQU87S0FPM0I7SUFQRCwwQkFPQztJQUx1QjtRQUR0QixhQUFHLENBQUMsUUFBUSxDQUFpQixvQ0FBa0IsQ0FBQzswQ0FDTTtJQUVoQztRQUR0QixhQUFHLENBQUMsR0FBRyxDQUFDLG9DQUFrQixDQUFDOzhCQUNJO0lBRVQ7UUFEdEIsYUFBRyxDQUFDLFFBQVEsQ0FBYSxhQUFhLENBQUM7c0NBQ08ifQ==
--- out/action/ToggleNPCsDisableAttack.js
+++ out/action/ToggleNPCsDisableAttack.js
@@ -1,3 +1,13 @@
+/*!
+ * Copyright 2011-2023 Unlok
+ * https://www.unlok.ca
+ *
+ * Credits & Thanks:
+ * https://www.unlok.ca/credits-thanks/
+ *
+ * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
+ * https://github.com/WaywardGame/types/wiki
+ */
 define(["require", "exports", "game/entity/action/Action", "game/entity/action/IAction", "game/entity/IEntity", "../action/IAction"], function (require, exports, Action_1, IAction_1, IEntity_1, IAction_2) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -8,4 +18,4 @@ define(["require", "exports", "game/entity/action/Action", "game/entity/action/I
         IAction_2.default.BALANCING_TOOLS.saveData.disableAttackNPCs = disable;
     });
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVG9nZ2xlTlBDc0Rpc2FibGVBdHRhY2suanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvYWN0aW9uL1RvZ2dsZU5QQ3NEaXNhYmxlQXR0YWNrLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztJQVFBLGtCQUFlLElBQUksZUFBTSxDQUFDLHdCQUFjLENBQUMsT0FBTyxDQUFDO1NBQy9DLFdBQVcsQ0FBQyxvQkFBVSxDQUFDLEtBQUssQ0FBQztTQUM3QixhQUFhLENBQUMsR0FBRywwQkFBZ0IsQ0FBQztTQUNsQyxVQUFVLENBQUMsQ0FBQyxNQUFNLEVBQUUsT0FBTyxFQUFFLEVBQUU7UUFDL0IsaUJBQU8sQ0FBQyxlQUFlLENBQUMsUUFBUSxDQUFDLGlCQUFpQixHQUFHLE9BQU8sQ0FBQztJQUM5RCxDQUFDLENBQUMsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVG9nZ2xlTlBDc0Rpc2FibGVBdHRhY2suanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvYWN0aW9uL1RvZ2dsZU5QQ3NEaXNhYmxlQXR0YWNrLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOzs7Ozs7Ozs7R0FTRzs7OztJQVVILGtCQUFlLElBQUksZUFBTSxDQUFDLHdCQUFjLENBQUMsT0FBTyxDQUFDO1NBQy9DLFdBQVcsQ0FBQyxvQkFBVSxDQUFDLEtBQUssQ0FBQztTQUM3QixhQUFhLENBQUMsR0FBRywwQkFBZ0IsQ0FBQztTQUNsQyxVQUFVLENBQUMsQ0FBQyxNQUFNLEVBQUUsT0FBTyxFQUFFLEVBQUU7UUFDL0IsaUJBQU8sQ0FBQyxlQUFlLENBQUMsUUFBUSxDQUFDLGlCQUFpQixHQUFHLE9BQU8sQ0FBQztJQUM5RCxDQUFDLENBQUMsQ0FBQyJ9
