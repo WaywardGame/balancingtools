8f2ee73c9d704350f88ff2dfd39467cac9d62803
Spacetech <spacetech326@gmail.com>
2023-06-15 05:04:17 -0700

    Update for 2.13.3-beta.dev.20230615.1

--- src/action/SpawnCreatureLine.ts
+++ src/action/SpawnCreatureLine.ts
@@ -14,21 +14,73 @@ import { CreatureType } from "game/entity/creature/ICreature";
 import { EntityType } from "game/entity/IEntity";
 import { TerrainType } from "game/tile/ITerrain";
 import Enums from "utilities/enum/Enums";
+import { creatureDescriptions } from "game/entity/creature/Creatures";
+import { DoodadType } from "game/doodad/IDoodad";
 
 export default new Action()
 	.setUsableBy(EntityType.Human)
 	.setHandler(action => {
 		const executor = action.executor;
 
-		// Spawn creatures in a line
-		for (const creatureType of Enums.values(CreatureType)) {
+		const biomeType = executor.island.biomeType;
+
+		// Spawn creatures in a line in order of spawning rep for the current biome type.
+		// if they don't have a spawnsOnReputation, order if they can spawn in the biome.
+		// everyone else goes at the end of the line.
+		const creatureTypes = Array.from(Enums.values(CreatureType))
+			.sort((creatureA, creatureB) => {
+				const creatureASort = creatureDescriptions[creatureA]?.spawn?.[biomeType]?.spawnsOnReputation ??
+					(creatureDescriptions[creatureA]?.spawnGroup?.[biomeType] ? -99998 : -99999);
+				const creatureBSort = creatureDescriptions[creatureB]?.spawn?.[biomeType]?.spawnsOnReputation ??
+					(creatureDescriptions[creatureB]?.spawnGroup?.[biomeType] ? -99998 : -99999);
+				return creatureBSort - creatureASort;
+			});
+
+		let yOffset = 1;
+		let addedSpawnsOnRepFence = false;
+		let addedspawnGroupFence = false;
+
+		for (const creatureType of creatureTypes) {
 			const x = executor.x + 2;
-			const y = executor.y + creatureType;
+			let y = executor.y + yOffset;
+
+			let createFence = false;
+
+			// add some fences to separate the categories: [ spawnsOnReputation | spawnGroup | others ]
+			if (creatureDescriptions[creatureType]?.spawn?.[biomeType]?.spawnsOnReputation === undefined) {
+				if (!addedSpawnsOnRepFence) {
+					addedSpawnsOnRepFence = true;
+					createFence = true;
+
+				} else if (!addedspawnGroupFence && creatureDescriptions[creatureType]?.spawnGroup?.[biomeType] === undefined) {
+					addedspawnGroupFence = true;
+					createFence = true;
+				}
+
+				if (createFence) {
+					for (var i = -1; i <= 2; i++) {
+						const tile = executor.island.getTileSafe(x + i, y, executor.z);
+						if (!tile) {
+							break;
+						}
+
+						if (tile.doodad) {
+							executor.island.doodads.remove(tile.doodad);
+						}
+
+						tile.changeTile(TerrainType.Dirt, false);
+						executor.island.doodads.create(DoodadType.WoodenFence, tile);
+					}
+
+					y++;
+					yOffset++;
+				}
+			}
 
 			for (var i = -1; i <= 2; i++) {
-				const tile = executor.island.getTile(x + i, y, executor.z);
+				const tile = executor.island.getTileSafe(x + i, y, executor.z);
 				if (!tile) {
-					continue;
+					break;
 				}
 
 				if (tile.doodad) {
@@ -40,6 +92,8 @@ export default new Action()
 
 			executor.island.creatures.spawn(creatureType, executor.island.getTile(x, y, executor.z), true, undefined, undefined, true);
 			executor.island.creatures.spawn(creatureType, executor.island.getTile(x + 1, y, executor.z), true, true, undefined, true);
+
+			yOffset++;
 		}
 
 		renderers.computeSpritesInViewport(action.executor);
